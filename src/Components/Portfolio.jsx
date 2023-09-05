063adac9c6cf6a7ad4adda70e89734fcb6da37c2
Mahtab-chelaniii <mahtabchelani@gmail.com>
2023-09-05 14:16:11 -0700

    add changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/birds.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "a lot of birds in the sky";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My linkedin profile 🎉",
     description:
       "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+    url: "https://www.linkedin.com/in/jmrchelani/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "My github Account",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "my GITHUB account",
+    url: "https://github.com/Mahtab-ch",
   },
   {
-    title: "My Resume Site",
+    title: "Stack overflow",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Stack overflow profile",
+    url: "https://stackoverflow.com/users/14437274/milton",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "youtube video",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Technical onboarding videos",
+    url: "https://www.youtube.com/watch?v=tl4vk64enKA",
   },
 ];
 
